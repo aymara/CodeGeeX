5f19f481dce7cb9475e2722b6af8499dd7319295
Gaël de Chalendar <gael.de-chalendar@cea.fr>
2023-11-29 17:12:52 +0100

    Allow to set temporary dir

--- scripts/evaluate_humaneval_x.sh
+++ scripts/evaluate_humaneval_x.sh
@@ -8,7 +8,7 @@ TIMEOUT=$4  # Timeout in seconds.
 SCRIPT_PATH=$(realpath "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 MAIN_DIR=$(dirname "$SCRIPT_DIR")
-
+TMP_DIR="${TMP_DIR:=/tmp}"
 echo "$INPUT_FILE"
 
 if [ -z "$N_WORKERS" ]
@@ -26,7 +26,8 @@ then
     TIMEOUT=5
 fi
 
-DATA_DIR=$MAIN_DIR/codegeex/benchmark/humaneval-x/$LANGUAGE/data/humaneval_$LANGUAGE.jsonl.gz
+DATA_DIR=$MAIN_DIR/codegeex/benchmark/humaneval-x/$LANGUAGE/data
+DATA_FILE=$DATA_DIR/humaneval_$LANGUAGE.jsonl.gz
 
 if [ $LANGUAGE = go ]; then
   export PATH=$PATH:/usr/local/go/bin
@@ -39,8 +40,8 @@ fi
 CMD="python $MAIN_DIR/codegeex/benchmark/humaneval-x/evaluate_humaneval_x.py \
     --input-file "$INPUT_FILE" \
     --n-workers $N_WORKERS \
-    --tmp-dir $MAIN_DIR/codegeex/benchmark/humaneval-x/ \
-    --problem-file $DATA_DIR \
+    --tmp-dir $TMP_DIR \
+    --problem-file $DATA_FILE \
     --timeout $TIMEOUT \
     --drop-prompt"  # To remove prompt from hypothesis if present
 
